e0e44b355c03c7acc3e214644de1c1c10436263e
josepheisenberg <jeisenbe@purdue.edu>
2024-12-09 23:24:35 -0500

    test

--- main.cpp
+++ main.cpp
@@ -12,7 +12,7 @@ std::optional<double> poly_test(polynomial& p1,
 {
     std::chrono::steady_clock::time_point begin = std::chrono::steady_clock::now();
 
-    polynomial p3 = p1 * p2;
+    polynomial p3 = p1 % p2;
 
     auto p3_can_form = p3.canonical_form();
 
@@ -22,6 +22,9 @@ std::optional<double> poly_test(polynomial& p1,
 
     if (p3_can_form != solution)
     {
+        for(auto & it : p3_can_form){
+            std::cout << it.first << ", " << it.second << std::endl;
+        }
         return std::nullopt;
     }
 
@@ -61,19 +64,22 @@ void modulo_test() {
 // int main()
 // {
     
-//     addition_test1();
-//     zero_poly_test();
-//     int_addition_test();
+//     // addition_test1();
+//     // zero_poly_test();
+//     // int_addition_test();
 //     modulo_test();
 //     //We're doing (x+1)^2, so solution is x^2 + 2x + 1
-//     std::vector<std::pair<power, coeff>> solution = {{2,1}, {1,2}, {0,1}};
+//     std::vector<std::pair<power, coeff>> solution = {{0,0}};
 
 //     // This holds (x+1), which we'll pass to each polynomial
-//     std::vector<std::pair<power, coeff>> poly_input = {{1,1}, {0,1}};
+//     std::vector<std::pair<power, coeff>> poly_input2 = {{1,1}, {0,1}};
 
-//     polynomial p1(poly_input.begin(), poly_input.end());
-//     polynomial p2(poly_input.begin(), poly_input.end());
-//     polynomial p3 = p1 * p2;
+    
+//     std::vector<std::pair<power, coeff>> poly_input1 = {{3, 2}, {2,5}, {1,7}, {0, 4}};
+
+//     polynomial p1(poly_input2.begin(), poly_input2.end());
+//     polynomial p2(poly_input2.begin(), poly_input2.end());
+//     polynomial p3 = p1 % p2;
 //     std::optional<double> result = poly_test(p1, p2, solution);
 
 //     if (result.has_value())
